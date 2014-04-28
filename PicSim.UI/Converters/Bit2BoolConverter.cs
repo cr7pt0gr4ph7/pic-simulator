ea1c28a9caf252029755fb97c2dfd9a324b758cf
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2014-04-28 14:18:09 +0200

    Add the class Bit2BoolConverter.

---
+++ PicSim.UI/Converters/Bit2BoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PicSim.UI.Converters
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class Bit2BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((bool)value) return "1";
+            else return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((value as string) == "1") return true;
+            else if ((value as string) == "0") return false;
+            else return Binding.DoNothing;
+        }
+    }
+}
