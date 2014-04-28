0cada12d9c2a866f06cd460955a5602c78b5f445
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2014-04-28 11:43:15 +0200

    Refactor the Communication class.

---
+++ PicSim/Components/Communication/CommPortInfo.cs
@@ -0,0 +1,34 @@
+﻿using PicSim.Components.Registers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSim.Components.Communication
+{
+    /// <summary>
+    /// This class models a communcation port (PORTA, PORTB, ...) which has a value register and a tristate register.
+    /// </summary>
+    public class CommPortInfo
+    {
+        private readonly IRegister m_valueRegister;
+        private readonly IRegister m_trisRegister;
+
+        public CommPortInfo()
+        {
+            m_valueRegister = new MemoryCell();
+            m_trisRegister = new MemoryCell();
+        }
+
+        public IRegister ValueRegister
+        {
+            get { return m_valueRegister; }
+        }
+
+        public IRegister TrisRegister
+        {
+            get { return m_trisRegister; }
+        }
+    }
+}
--- PicSim/Components/Communication/Communication.cs
+++ PicSim/Components/Communication/Communication.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NLog;
+using PicSim.Components.Registers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,55 +8,75 @@ using System.Threading.Tasks;
 
 namespace PicSim.Components.Communication
 {
-    class Communication
+    public class Communication
     {
-        const uint CR = 0x0D;
+        private static readonly Logger ms_logger = LogManager.GetCurrentClassLogger();
+        private const uint CARRIAGE_RETURN = 0x0D;
 
-        private RS232 m_backingPort;
-        private CommunicationData[] tris;
-        private CommunicationData[] port;
+        private RS232 m_connection;
+        private CommPortInfo[] m_ports;
 
         public Communication()
         {
-            m_backingPort = new RS232();
-            tris = new CommunicationData[2];
-            port = new CommunicationData[2];
+            m_connection = new RS232();
+
+            var portA = new CommPortInfo();
+            var portB = new CommPortInfo();
+
+            PORTA = portA.ValueRegister;
+            TRISA = portA.TrisRegister;
+
+            PORTB = portB.ValueRegister;
+            TRISB = portB.TrisRegister;
+
+            m_ports = new CommPortInfo[] { portA, portB };
         }
 
-        public void start()
+        public IRegister PORTA { get; private set; }
+        public IRegister PORTB { get; private set; }
+        public IRegister TRISA { get; private set; }
+        public IRegister TRISB { get; private set; }
+
+        public void Start()
         {
-            m_backingPort.Open();
+            m_connection.Open();
         }
 
-        public void stop()
+        public void Stop()
         {
-            m_backingPort.Close();
+            m_connection.Close();
         }
 
         public void Sync()
         {
-            for(uint i = 0; i < port.Length ; i++)
+            foreach (CommPortInfo port in m_ports)
             {
-                m_backingPort.WriteValue(tris[i].GetUpperNibble());
-                m_backingPort.WriteValue(tris[i].GetLowerNibble());
+                m_connection.WriteValue(port.TrisRegister.GetUpperNibble());
+                m_connection.WriteValue(port.TrisRegister.GetLowerNibble());
 
-                m_backingPort.WriteValue(port[i].GetUpperNibble());
-                m_backingPort.WriteValue(port[i].GetLowerNibble());
-                m_backingPort.WriteValue(CR); // CR
+                m_connection.WriteValue(port.ValueRegister.GetUpperNibble());
+                m_connection.WriteValue(port.ValueRegister.GetLowerNibble());
+                m_connection.WriteValue(CARRIAGE_RETURN);
             }
 
             uint j = 0;
             byte data = 0;
             do
             {
-                data = (byte) m_backingPort.ReadValue();
-                if (data == CR)
-                    break;
-                port[j].SetUpperNibble(data);
-                data = (byte)m_backingPort.ReadValue();
-                port[j].SetLowerNibble(data);
+                // Read upper nibble
+                data = (byte)m_connection.ReadValue();
+                if (data == CARRIAGE_RETURN) break;
+
+                if (j < m_ports.Length) m_ports[j].ValueRegister.SetUpperNibble(data);
+                else ms_logger.Warn("Skipping out-of-range port {0}", j);
+
+                // Read lower nibble
+                data = (byte)m_connection.ReadValue();
+                if (j < m_ports.Length) m_ports[j].ValueRegister.SetLowerNibble(data);
+                else /* Warning was already written above */;
+
                 j++;
-            } while (data != CR);
+            } while (data != CARRIAGE_RETURN);
 
         }
     }
