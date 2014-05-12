c45077b7f4015507b0cf178299896ff2f00c5619
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2014-05-12 09:54:39 +0200

    Close the frequency controller window on pressing the escape key.

---
+++ PicSim.UI/Behaviors/CloseOnEscapeBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace PicSim.UI.Behaviors
+{
+    public class CloseOnEscapeBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewKeyDown += AssociatedObject_PreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewKeyDown -= AssociatedObject_PreviewKeyDown;
+        }
+
+        void AssociatedObject_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                AssociatedObject.Close();
+                e.Handled = true;
+            }
+        }
+    }
+}
